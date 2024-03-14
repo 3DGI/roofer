3cff7a2286d71a5f988ea3db53cfba3ea126cb9d
Ravi Peters <ravi@ylan.nl>
2024-03-14 12:44:49 +0100

    add PointCloudReader

---
+++ src/io/PointCloudReader.hpp
@@ -0,0 +1,25 @@
+
+#include "../datastructures.hpp"
+#include "../projHelper.hpp"
+#include <memory>
+
+namespace roofer {
+  struct PointCloudReaderInterface {
+
+    projHelperInterface& pjHelper;
+
+    PointCloudReaderInterface(projHelperInterface& pjh) : pjHelper(pjh) {};
+
+    virtual void open(const std::string& source) = 0;
+
+    virtual void readPointCloud(
+        PointCollection& points, 
+        vec1i* classification=nullptr,
+        vec1i* order=nullptr,
+        vec1f* intensities=nullptr,
+        vec3f* colors=nullptr
+    ) = 0;
+  };
+
+  std::unique_ptr<PointCloudReaderInterface> createPointCloudReaderLASlib(projHelperInterface& pjh);
+}
